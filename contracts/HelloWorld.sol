af6ed6a5b4b38741c5e46c1fa28f63c749738dc3
Hoang Huynh <huynh.hoang@dinovative.com>
2022-01-13 17:56:18 +0700

    done Hello World

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.10 and less than 0.9.0
+pragma solidity ^0.8.10;
+
+contract HelloWorld {
+    string public myName = "Hello World!";
+    // Add this function:
+    function getMyName() public view returns(string memory) {
+        return myName;
+    }
+  
+    function changeMyName(string memory _newName) public {
+        myName = _newName;
+    }
+}
